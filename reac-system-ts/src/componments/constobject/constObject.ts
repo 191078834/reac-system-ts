786a5c6334db8f01130e446bba4a2517a99c593c
191078834 <191078834@qq.com>
2022-10-25 12:00:32 +0900

    set grid params

---
+++ reac-system-ts/src/componments/constobject/constObject.ts
@@ -0,0 +1,55 @@
+import { format } from 'date-fns'
+import { ja } from 'date-fns/locale'
+
+export const wordListTitle = {
+    id: 'NO.',
+    word: '単語',
+    hinagara: 'ローマ字',
+    translate: '中国語',
+    putTime: '記入時間'
+}
+
+export const kinyuTableColumns = [
+    { field: 'id', headerName: 'NO.', width: 50 },
+    {
+        field: 'word',
+        headerName: '単語',
+        width: 200,
+        editable: true
+    },
+    {
+        field: 'loumaji',
+        headerName: 'ローマ字',
+        // type: 'singleSelect',
+        width: 200,
+        editable: true
+    },
+    {
+        field: 'translate',
+        headerName: '翻訳',
+        type: 'singleSelect',
+        valueOptions: ['full time', 'part time', 'intern'],
+        width: 400,
+        editable: true
+    },
+    {
+        field: 'putTime',
+        headerName: '記入時間',
+        type: 'date',
+        width: 200,
+        valueFormatter: (params: any): String => {
+
+            let year = stringToNumber(params.value.slice(0, 4));
+            let month = stringToNumber(params.value.slice(5, 7)) - 1;
+            let day = stringToNumber(params.value.slice(8, 10));
+            let itemDate = new Date(year, month, day);
+
+            return format(itemDate, 'yyyy/MM/dd(eee)', { locale: ja })
+        }
+    },
+];
+
+const stringToNumber = (str: String): number => Number(str);
+
+
+
