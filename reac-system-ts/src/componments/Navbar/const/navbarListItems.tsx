324770ca5dc0d66f63022c8af02345e092b34bc7
191078834 <191078834@qq.com>
2022-10-25 14:27:29 +0900

    set grid params

---
+++ reac-system-ts/src/componments/Navbar/const/navbarListItems.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import SpellcheckIcon from '@mui/icons-material/Spellcheck';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
+
+interface mainNavBarItem{
+    id: number,
+    icon: any,   
+    label: String,
+    route: String
+}
+const mainNavBarItems:Array<mainNavBarItem> = [
+    {
+        id: 0,
+        icon: <SpellcheckIcon />,
+        label: '単語リスト',
+        route: 'wordlist'
+
+
+    },
+    {
+        id: 1,
+        icon: <ArrowForwardIosIcon />,
+        label: '毎日単語',
+        route: 'everydaylist'
+
+
+    },
+    {
+        id: 2,
+        icon: <ArrowForwardIosIcon />,
+        label: '忘れた単語',
+        route: 'forgetlist'
+
+    }
+]
+
+
+export default mainNavBarItems
