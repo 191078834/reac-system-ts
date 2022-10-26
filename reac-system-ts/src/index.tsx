788644a15ddf31b2b510c3828568e05fa0a9f2cb
191078834 <191078834@qq.com>
2022-10-26 15:29:06 +0900

    ts

--- reac-system-ts/src/index.tsx
+++ reac-system-ts/src/index.tsx
@@ -1,15 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import App from './pages/App/App';
+import { ThemeProvider } from '@mui/material/styles';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import {dashBoardTheme} from "./dashBoardTheme"
+
+import App from './pages/App/App';
+import TestApp from './pages/TestApp/TestApp';
+import ForgetList from './pages/ForgetList/ForgetList';
+import WordList from'./pages/WordList/WordList';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+   <ThemeProvider theme={dashBoardTheme}>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />}>
+          <Route path="wordlist" element={<WordList />} />
+          <Route path="everydaylist" element={<TestApp />} />
+          <Route path="forgetlist" element={<ForgetList />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>,
+  </ThemeProvider>
+
   </React.StrictMode>
 );
 
