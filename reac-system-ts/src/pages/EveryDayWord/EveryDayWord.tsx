612a9bd9a805d153596e6cd948c422ea9ee31c2a
191078834@qq.com <191078834@qq.com>
2022-11-01 23:31:59 +0900

    react-hook-form を使用

--- reac-system-ts/src/pages/EveryDayWord/EveryDayWord.tsx
+++ reac-system-ts/src/pages/EveryDayWord/EveryDayWord.tsx
@@ -1,81 +1,88 @@
 import * as React from 'react'
 import Box from '@mui/material/Box';
+import { useForm, Controller } from "react-hook-form";
+
 import { Button, Grid, TextField } from '@mui/material';
 
+interface IFormInput{
+    word: string,
+    lomazi: string,
+    translate: string
+};
 const EveryDayWord: React.FC<{}> = () => {
-    let wordRef = React.useRef<HTMLInputElement>(null);
-    let translateRef = React.useRef<HTMLInputElement>(null);
-    const [hatuo, setHatuo] = React.useState<String>("");
+
+    const { control, handleSubmit } = useForm<IFormInput>();
+    const [submitDisabled, setSubmitDisabled] = React.useState<boolean>(true);
+
+
+    // let wordRef = React.useRef<HTMLInputElement|undefined>(undefined);
+   
     // const [ajaxUrl, setAjaxUrl] = React.useState<String>("");
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-
-        e.preventDefault();
-        console.log(wordRef.current?.value, hatuo, translateRef.current?.value)
-        // alert(wordRef.current?.value);
-    }
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setHatuo(e.target.value);
-    }
-    const handleOnblur = (e: React.FocusEvent<HTMLInputElement>) => {
-        e.preventDefault()
-        fetch("http://localhost:8090/everydayword/wordcheck", {
-            method: "POST",
-            mode: "cors",
-            headers: {
-                "Accept": "application/json,text/plain,*/*",
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ word: wordRef.current?.value })
-        })
+    const onSubmit = (data: IFormInput)=>console.log(data);
+    const handlerOnBlur = (data: IFormInput)=>(data.word.length<1||data.lomazi.length<1||data.translate.length<1)? setSubmitDisabled(true): setSubmitDisabled(false)
         
-    }
+       
+       
+    
+    // const handleOnblur = (e: React.FocusEvent<HTMLInputElement>) => {
+    //     e.preventDefault()
+    //     fetch("http://localhost:8090/everydayword/wordcheck", {
+    //         method: "POST",
+    //         mode: "cors",
+    //         headers: {
+    //             "Accept": "application/json,text/plain,*/*",
+    //             "Content-Type": "application/json"
+    //         },
+    //         body: JSON.stringify({ word: 1 })
+    //     })
+
+    // }
 
 
 
     return (
 
-        <Box component="form" onSubmit={handleSubmit} sx={{ maxWidth: '100%' }}>
+        <Box component="form" onSubmit={handleSubmit(onSubmit)} sx={{ maxWidth: '100%' }}>
             <Grid container spacing={1} >
                 {/* 1行目 */}
                 <Grid item xs={4}>
-                    <TextField
-                        name='word'
-                        required
-                        label="単語"
-                        inputRef={wordRef}
-                        onBlur={handleOnblur}
+                <Controller
+                    render={({ field }) => <TextField {...field} onBlur={handleSubmit(handlerOnBlur)}  label="単語"/>}
+                    name="word"
+                    
+                    control={control}
+                    defaultValue=""
                     />
                 </Grid>
                 <Grid item xs={5}>
-                    <TextField
+                    <Controller
+                        render={({ field }) => <TextField {...field} required label="発音" onBlur={handleSubmit(handlerOnBlur)} />}
                         name='lomazi'
-                        required
-                        onChange={handleChange}
-                        label="発音"
-                        value={hatuo}
+                        defaultValue=""
+                        control={control}
                     />
                 </Grid>
                 {/* 2行目 */}
                 <Grid item xs={9}>
-                    <TextField
+                    <Controller
                         name='translate'
-                        required
-                        label="翻訳"
-                        sx={{ width: 600, mt: 2 }}
-                        inputRef={translateRef}
+                        defaultValue=""
+                        render={({ field }) => <TextField {...field} required sx={{ width: 700, mt: 2 }} label="翻訳" onBlur={handleSubmit(handlerOnBlur)} />}
+                        control={control}
                     />
                 </Grid>
                 <Grid item xs={3}>
                 </Grid>
                 {/* 3行目 */}
-                <Grid item xs={6}>
+                <Grid item xs={7}>
                 </Grid>
                 <Grid item xs={1}>
                     <Button
                         type="submit"
                         fullWidth
                         variant="contained"
+                        disabled={submitDisabled}
                         sx={{ mt: 1, ml: 20, height: 55 }}
                     >
                         確認
@@ -83,6 +90,7 @@ const EveryDayWord: React.FC<{}> = () => {
                 </Grid>
             </Grid>
         </Box>
+      
 
     )
 }
