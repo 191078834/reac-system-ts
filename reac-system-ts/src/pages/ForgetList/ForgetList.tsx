2f3bb1683346181c8850a3731336f0f015f01ff8
191078834 <191078834@qq.com>
2022-11-11 11:24:06 +0900

    Array 操作

--- reac-system-ts/src/pages/ForgetList/ForgetList.tsx
+++ reac-system-ts/src/pages/ForgetList/ForgetList.tsx
@@ -20,9 +20,9 @@ let spase2: string = ':'
 let timeTostring = year + space + month + space + day + '   ' + hours + spase2 + minutes + spase2 + seconds
 let arrayNames: Array<iData> = [
     { firstname: 'first', lastname: 'first', currend: timeTostring },
-    { firstname: 'wawafddng', lastname: 'rmali', currend: timeTostring },
-    { firstname: 'wdeggang', lastname: 'ma58li', currend: timeTostring },
-    { firstname: 'wandfag', lastname: 'maggli', currend: timeTostring }
+    { firstname: 'second', lastname: 'rmali', currend: timeTostring },
+    { firstname: 'third', lastname: 'ma58li', currend: timeTostring },
+    { firstname: 'forteen', lastname: 'maggli', currend: timeTostring }
 ]
 
 const ForgetList: React.FC<{}> = () => {
@@ -41,11 +41,19 @@ const ForgetList: React.FC<{}> = () => {
         console.log("push() : 返回添加后的数组长度 ", arrayNames.push({ firstname: 'new', lastname: 'new', currend: timeTostring }))
         console.log("pop()删除最后一个元素，返回删除项", arrayNames.pop())
         console.log("shift()删除第一个元素，返回删除项", arrayNames.shift())
-        console.log("unshift()接收任意数量参数，将它们添加到数组开头, 返回最新数组长度", arrayNames.unshift({ firstname: 'new3', lastname: 'new3', currend: timeTostring }))
-        console.log("reverse() 将数组元素反向排序", arrayNames.reverse())
-        console.log()
-        console.log()
-        console.log()
+        console.log("unshift()接收任意数量参数，将它们添加到数组开头, 返回最新数组长度", arrayNames.unshift({ firstname: 'firstttt', lastname: 'new3', currend: timeTostring }))
+        // console.log("reverse() 将数组元素反向排序", arrayNames.reverse())
+        // sort 比较函数
+        arrayNames.sort((a, b) => a.firstname.length < b.firstname.length ? -1 : a.firstname.length > b.firstname.length ? 1 : 0)
+        console.log("sort() 将数组元素排序", arrayNames)
+
+        console.log('下面方法不改变原数组在现有数组全部元素基础上创建一个新数组')
+        let newArray = arrayNames.concat({ firstname: 'sasin', lastname: 'sasin', currend: timeTostring })
+        console.log('concat()它的参数添加到副本末尾，最后返回这个新构建的数组', newArray)
+        let newArray2 = arrayNames.slice(1, 3)
+        console.log('slice()参数：①返回元素开始索引，②结束索引,复制特定一段的数组arg1<newArray<=arg2', newArray2)
+        // 等待更新
+        console.log('splice()会改变原数组')
         console.log()
         console.log()
 
